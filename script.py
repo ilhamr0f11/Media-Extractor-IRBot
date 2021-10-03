0b542a8421580810d5e14ef083411e17c1d9d47a
Ilham Rofi'i <70249693+ilhamr0f11@users.noreply.github.com>
2021-10-03 10:42:52 +0700

    Add files via upload

---
+++ script.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @trojanzhex
+
+
+class Script(object):
+
+    START_MSG = """<b>Hello {},
+
+I can extract audio and subtitles from video files.
+
+Send me any video file and extract what you want :).
+
+See <i>help</i> for more details.
+
+@irbotsupdate</b>"""
+
+
+    HELP_MSG = """<b>Hai, Follow these Steps..</b>
+
+🌀 <i>Send me any Valid video file.</i>
+
+🌀 <i>Click Download and Process button to download the file to my server</i>
+
+🌀 <i>Wait till I process the video!</i>
+
+🌀 <i>Now select the stream you need to extract!</i>
+
+<b>© @mediaextractorirbot</b>"""
+
+
+    ABOUT_MSG = """⭕️<b>My Name : Media Extractor IR Bot</b>
+
+⭕️<b>Creater :</b> @irbotsupdate    
+
+⭕️<b>Language :</b> <code>Python3</code>
+
+⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+
+"""
